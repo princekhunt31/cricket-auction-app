65407170dc72e20addfcc9f279b55fffca5ddbde
princekhunt31 <pkhunt04@gmail.com>
2026-04-10 19:58:24 +0530

    fix: remove SSR hydration for static deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -20,7 +20,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
+   // provideClientHydration(withEventReplay()),
     provideAnimations(),
     {
       provide: APP_INITIALIZER,
